25079c4e68b24ec503da3e3f59effcbd00cb02cb
sanxun <sanxun@qq.com>
2021-11-11 01:39:18 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author qpt
+ * @create 2021-11-11 1:32
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
